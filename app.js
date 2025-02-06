d06d516eee649fb973fc711f75266c7954f8562f
Juliano Antonio Silva <julianoff243@gmail.com>
2025-02-06 08:07:36 -0300

    Atribuindo valor a lista

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 let amigos = []; //Criamos uma lista, por ter valor igual a "[]" siginifica que a variável se trata de uma lista
-function adicionarAmigo(){}
+const inputAmigo = document.getElementById("amigo"); //Pegamos o input do HTML pelo ID
+const listaAmigo = document.getElementById("listaAmigos");
+
+function adicionarAmigo() {
+    if (inputAmigo.value === "") {
+        alert("Digite o nome do amigo");
+        return 
+    } 
+    amigos.push(inputAmigo.value)
+    const itenAmigos = document.createElement("li")
+    itenAmigos.innerHTML = inputAmigo.value
+    listaAmigo.appendChild(itenAmigos)
+}
+
