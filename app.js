5bdb0a0589b34118a5f5ab093f49c197f4f0708a
Juliano Antonio Silva <julianoff243@gmail.com>
2025-02-06 20:49:27 -0300

    Limpando o campo de entrada

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-let amigos = []; //Criamos uma lista, por ter valor igual a "[]" siginifica que a variável se trata de uma lista
-const inputAmigo = document.getElementById("amigo"); //Pegamos o input do HTML pelo ID
-const listaAmigo = document.getElementById("listaAmigos");
+let amigos = []; // Criamos uma lista
+
+const inputAmigo = document.querySelector("#amigo"); // Pegamos o input do HTML pelo ID
+const listaAmigo = document.querySelector("#listaAmigos"); // Pegamos a lista pelo ID
 
 function adicionarAmigo() {
-    if (inputAmigo.value === "") {
-        alert("Digite o nome do amigo");
-        return 
-    } 
-    amigos.push(inputAmigo.value)
-    const itenAmigos = document.createElement("li")
-    itenAmigos.innerHTML = inputAmigo.value
-    listaAmigo.appendChild(itenAmigos)
-}
+  if (inputAmigo.value === "") {
+    alert("Por favor, insira um nome.");
+    return;
+  }
+  amigos.push(inputAmigo.value);
+  const itenAmigos = document.createElement("li");
+  itenAmigos.innerHTML = inputAmigo.value;
+  listaAmigo.appendChild(itenAmigos);
 
+  inputAmigo.value = ""
+}
